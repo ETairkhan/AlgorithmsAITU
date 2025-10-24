2a4d3450b0940ef214f4ab67340cf9df337c9127
ETairkhan <trouzerop@gmail.com>
2025-10-24 15:28:53 +0500

    graphs asssigment3

---
+++ src/Assignment3/Graph.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Graph {
+    List<String> nodes;
+    List<Edge> edges;
+
+    public Graph(List<String> nodes, List<Edge> edges) {
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public static class Edge {
+        String from, to;
+        int weight;
+
+        public Edge(String from, String to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+}
